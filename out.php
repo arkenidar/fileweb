70a72d569cbd5414769bf4fcac0e079097ea3f0e
Dario Cangialosi <dario.cangialosi@gmail.com>
2020-02-10 15:20:53 +0100

    htmlspecialchars security fix

--- out.php
+++ out.php
@@ -3,5 +3,5 @@ $filename="comments";
 $array=json_decode(file_get_contents("$filename.json"));
 foreach($array as $item){
 $name=$item[0]; $says=$item[1];
-echo "$name says $says <br>\n";
+echo htmlspecialchars("$name says $says <br>\n");
 }
