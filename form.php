0b5dd792cde581a177525f27bed726cae9259c93
Dario Cangialosi <dario.cangialosi@gmail.com>
2020-02-10 17:04:18 +0000

    exclusive lock

--- form.php
+++ form.php
@@ -2,5 +2,5 @@
 $filename="comments";
 $array=json_decode(file_get_contents("$filename.json"));
 $array[]=['added',$_REQUEST["line"]];
-file_put_contents("$filename.json",json_encode($array));
+file_put_contents("$filename.json",json_encode($array),LOCK_EX);
 include 'file.php';
